14e91b6e1f9f6304d2a4f2b555413c3a7e742046
Éloïse Zappala <eloise.zappala@proton.me>
2025-09-19 09:44:21 -0400

    chore: delete old default file

--- home/linux/gui/hyprland/default.nix
+++ home/linux/gui/hyprland/default.nix
@@ -1,45 +1,60 @@
 {
-  pkgs,
-  config,
-  lib,
   ...
-}@args:
-let
-  cfg = config.modules.desktop.hyprland;
-in
+}:
 {
-  options.modules.desktop.hyprland = {
-    enable = lib.mkEnableOption "hyprland compositor";
-    settings = lib.mkOption {
-      type =
-        with lib.types;
-        let
-          valueType =
-            nullOr (oneOf [
-              bool
-              int
-              float
-              str
-              path
-              (attrsOf valueType)
-              (listOf valueType)
-            ])
-            // {
-              description = "Hyprland configuration value";
-            };
-        in
-        valueType;
-      default = { };
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    # must use nixos global module (~/sys/modules/desktop/hypr-global)
+    package = null;
+    portalPackage = null;
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, F, exec, zen"
+        "$mod, T, exec, ghostty"
+        "$mod, F12, exec, reboot"
+        "$mod, Q, killactive"
+        "$mod SHIFT, Q, forcekillactive"
+        "$mod, M, exit"
+        "$mod, J, movefocus, d"
+        "$mod, K, movefocus, u"
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod SHIFT, J, movewindow, d"
+        "$mod SHIFT, K, movewindow, u"
+        "$mod SHIFT, H, movewindow, l"
+        "$mod SHIFT, L, movewindow, r"
+      ];
+
+      monitor = [ ", highres, auto, 1" ];
+      animation = [
+        "workspaces, 1, 5, default, slidevert"
+      ];
+
+      input = {
+        kb_layout = "us";
+        kb_options = "caps:escape,compose:menu";
+        follow_mouse = 1;
+        sensitivity = 0;
+        touchpad = {
+          natural_scroll = true;
+          tap-to-click = false;
+          clickfinger_behavior = 1;
+        };
+      };
+
+      cursor = {
+        hide_on_key_press = true;
+      };
+
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_min_fingers = 3;
+      };
     };
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      {
-        wayland.windowManager.hyprland.settings = cfg.settings;
-      }
-      (import ./hyprland.nix args)
-      (import ./xdg.nix args)
-    ]
-  );
+  # let electron apps use wayland
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
